a303247da91ff3b65a29a88f2630c0550fa6bb7b
Matteo Maddalena <d.maddalena69@gmail.com>
2026-08-22 22:08:12 +0200

    scritti connection.c e logger.c per testare il client

--- src/server/connection.c
+++ src/server/connection.c
@@ -1,4 +1,20 @@
+#include "lib/header/counter.h"
+#include "lib/header/logbuffer.h"
+#include <pthread.h>
+#include <unistd.h>
+
+struct thread_in {
+    struct server_conf* server_conf;
+    int connection_sfd;
+    counter* threads_count;
+    logbuffer* logbuffer;
+    pthread_mutex_t* stdout_mux;
+    pthread_mutex_t* stderr_mux;
+};
+
 void* connection_handler(void* thread_in)
 {
-    
+    struct thread_in* in = (struct thread_in*)thread_in;
+    close(in->connection_sfd);
+    counter_down(in->threads_count);
 }
--- src/server/logger.c
+++ src/server/logger.c
@@ -1,3 +1,18 @@
+#include "lib/header/counter.h"
+#include "lib/header/logbuffer.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+struct thread_in {
+    struct server_conf* server_conf;
+    int connection_sfd;
+    counter* threads_count;
+    logbuffer* logbuffer;
+    pthread_mutex_t* stdout_mux;
+    pthread_mutex_t* stderr_mux;
+};
+
 bool flush_consumer(struct log log)
 {
     
@@ -5,5 +20,6 @@ bool flush_consumer(struct log log)
 
 void* flush_logbuffer(void* thread_in)
 {
-
+    struct thread_in* in = (struct thread_in*)thread_in;
+    counter_down(in->threads_count);
 }
